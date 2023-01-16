724c440cf039fcac16bf18b99f62885888d016fc
joyful97 <liccime97@gmail.com>
2023-01-16 09:03:04 -0800

    malloc checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 /**
- *  malloc_checked - allocates memory using malloc
- *  @b: the soze of allocated memory
+ * malloc_checked - allocates memory using malloc
+ * @b: the soze of allocated memory
  *
- *  Return: pointer to the allocated memory
+ * Return: pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
